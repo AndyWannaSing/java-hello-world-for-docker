29f6485953d384ac808dc34864d7c44faf6584f6
lq@jianzhimao.com <1281058343@qq.com>
2018-11-10 17:49:25 +0800

    添加项目代码

---
+++ src/main/java/hello/world/App.java
@@ -0,0 +1,15 @@
+package hello.world;
+
+/**
+ * Hello world!
+ *
+ * @author Andy
+ * @time 2018.11.10
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
